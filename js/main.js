34e6e6e546af306bd3ebe326a70e3f5e04043afb
davidabutler92 <davidabutler92@yahoo.com>
2020-04-01 16:43:18 -0700

    add user interface logic

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
     var inputPhrase = $("input#phrase").val();
-    console.log(inputPhrase);
-    var inputValues = inputPhrase.split(" ");
-    console.log("inputValues: ", inputValues);
+    // console.log(inputPhrase);
+    var  = pigLatin(inputPhrase);
+
+    $("#result").show();
+
+
   });
 });
+
